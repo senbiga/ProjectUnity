27dbef56e766558ce8fd53d11ec6b9c6e8f26e7e
Senbiga <sshenbiga@gmail.com>
2021-03-24 08:22:29 +0530

    First Commit

---
+++ src/main/java/pageObjects/ForgotPassword.java
@@ -0,0 +1,43 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ForgotPassword {
+
+	
+	public WebDriver driver;
+	
+	By email=By.cssSelector("[id='user_email']");
+	By sendMeInstructions=By.cssSelector("[type='submit']");
+	
+	
+	
+	
+	
+	public ForgotPassword(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		
+		this.driver=driver;
+		
+	}
+
+
+	public WebElement getEmail()
+	{
+		return driver.findElement(email);
+	}
+	
+	public WebElement sendMeInstructions()
+	{
+		driver.findElement(email).findElement(by)
+		return driver.findElement(sendMeInstructions);
+		
+	}
+	
+	
+
+	
+	
+}
